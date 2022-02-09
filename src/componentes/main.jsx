fe34a41895e56bc887bc3ba9c61dace7a9394e81
ozmarshall <parc.tec20@yahoo.com>
2022-02-09 18:08:55 -0500

    correcion main

--- src/componentes/main.jsx
+++ src/componentes/main.jsx
@@ -1,10 +1,17 @@
-export function Main() {
+export function Main(props) {
+  const {
+    children
+  } = props;
   return (
     <main className="mainM">
       <section>
         <input type="text" placeholder="Ubicación ?" />
         <input type="text" placeholder="Que buscas ?" />
+        {children}
       </section>
+      
     </main>
   );
 }
+
+
