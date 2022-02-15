d349d68ee29b0bb61b9b891bec22c9211929bef5
ozmarshall <parc.tec20@yahoo.com>
2022-02-15 18:47:14 -0500

    actualizando

--- src/paginas/contactanos.jsx
+++ src/paginas/contactanos.jsx
@@ -1,9 +1,11 @@
+
+
 export function Contactanos() {
   return (
     <div className="pagina_contacto">
       <p>aqui el contenido de contactanos</p>
       <div>
-        <img src="https://picsum.photos/200/300?grayscale" alt="" />
+        
       </div>
     </div>
   );
---
+++ src/ui/banners/banner_styles.js
@@ -0,0 +1,7 @@
+import banner_contactanos from "../banners/banner_contactanos.jpg";
+
+
+export default {
+    "img10" : banner_contactanos,
+    
+}
