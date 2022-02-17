bf989e44581942a78f7e6e2c2a38f93202e31ef6
ozmarshall <parc.tec20@yahoo.com>
2022-02-16 21:38:01 -0500

    actualizacion proyecto

--- src/componentes/header/header.jsx
+++ src/componentes/header/header.jsx
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import { useHistory } from "react-router-dom";
-=======
 import { Link } from "react-router-dom";
 
->>>>>>> 46ea94b7c7f0a2e777337ad3a56f54d77ec7df7f
 import { Input } from "ui/input/input";
 import { Nav } from "../nav/nav";
 import "./header.scss";
@@ -11,11 +8,6 @@ import "./header.scss";
 export function Header() {
   let history = useHistory();
   return (
-<<<<<<< HEAD
-    <header className="headerH">
-      <h1><a href="#" className="" onClick={() => {history.push("/pagina-principal");}}>DeTodo.com</a></h1>
-      <Input placeholder="Autos, Accesorios, restaurantes y mucho más..." className="w-2/5"/>
-=======
     <header className="headerH text-white">
       <h1>
         <Link to="/">deTodo.com</Link>
@@ -24,7 +16,6 @@ export function Header() {
         placeholder="Autos, Accesorios, restaurantes y mucho más..."
         className="w-2/5"
       />
->>>>>>> 46ea94b7c7f0a2e777337ad3a56f54d77ec7df7f
       <Nav />
     </header>
   );
--- src/paginas/registrate/registrate.jsx
+++ src/paginas/registrate/registrate.jsx
@@ -8,27 +8,27 @@ export function Registrate() {
   return (
     <div className="page-register text-center">
       <h1 className="mb-8">REGISTRATE</h1>
-      <form className="formulario w-80 mx-auto text-center">
+      <form className="formulario w-3/4 mx-auto text-center">
         <div>
           <Uploadimage className="w-96 h-96"></Uploadimage>
         </div>
         <div>
-          <Input placeholder="Apellido" className="w-full mb-8" />
-          <Input type="number" placeholder="Telefono" className="w-full mb-8" />
+          <Input placeholder="Apellido" className="w-3/4 mb-8" />
+          <Input type="number" placeholder="Telefono" className="w-3/4 mb-8" />
           <Input
             type="number"
             placeholder="Documento de Identidad"
-            className="w-full mb-8"
+            className="w-3/4 mb-8"
           />
-          <Input type="email" placeholder="E-mail" className="w-full mb-8" />
+          <Input type="email" placeholder="E-mail" className="w-3/4 mb-8" />
           <Input
             type="password"
             placeholder="Password"
-            className="w-full mb-8"
+            className="w-3/4 mb-8"
           />
         </div>
-        <div>
-          <AreaText cols="80" rows="10">
+        <div className="area_text">
+          <AreaText cols="110" rows="10">
             Cuentanos sobre ti ..
           </AreaText>
         </div>
