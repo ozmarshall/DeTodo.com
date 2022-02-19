61c9df9e34c8b935f6bb5456d2823f1752fb622c
ozmarshall <parc.tec20@yahoo.com>
2022-02-19 08:32:23 -0500

    PRESENTACION

--- src/paginas/detalle/detalle.jsx
+++ src/paginas/detalle/detalle.jsx
@@ -1,20 +1,52 @@
-import { useState } from "react";
-import  axios  from "axios";
-import { useHistory } from "react-router-dom";
-import { AreaText } from "ui/areaText/area-text";
-import { Input } from "ui/input/input";
 import { Button } from "ui/buttons/button";
-import { Uploadimage } from "ui/upload_image/upload-image";
 import iconos_styles from "../../ui/iconos/iconos_styles";
 import "./detalle.scss";
 
 export function Detalle() {
-  let history = useHistory();
-  
   return (
-    <div className="detalle max-w-screen-xl mx-auto my-10">
+    <div className="detalle_1 max-w-screen-xl mx-auto my-10">
       <section>
-
+        <div className="detalle_2">
+          <div className="detalle_3">
+            <div>
+              <h2>Colocar en mis Favoritos</h2>
+              <h2>Mandar al Carrito</h2>
+            </div>
+            <div className="w-64">
+              <p>
+                Tu anuncio se presentará asi, si esta correcto pulse boton
+                VENDER AHORA, si no pulse:
+              </p>
+              <Button>Modificar</Button>
+            </div>
+          </div>
+          <div>
+            <img
+              src="https://images.ecestaticos.com/aIGpEv4YaBeFT6H9Pky-erHP_1E=/0x0:640x361/1440x810/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fee7%2Fa9f%2F226%2Fee7a9f226660c503832fd08fcd638d75.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="detalle_4">
+          <p className="id_anuncio">ID del Anuncio: 7868675987</p>
+          <p className="id_precio text-green-500 py-8 text-9xl">S/ 2000</p>
+          <p>Bicicleta montañera negra</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+            adipisci doloremque suscipit eum deserunt est inventore, accusantium
+            similique unde animi iusto harum quaerat, quas necessitatibus
+            reiciendis aliquid totam ullam voluptatum!
+          </p>
+          <div className="detalle_5">
+            <p>Nuevo/Usado</p>
+            <p>Estado 1/10</p>
+            <p>Cantidad de articulos</p>
+            <p>Metodo de Pago</p>
+            <p>Punto de Entrega</p>
+            <p>Punto de Recepcion</p>
+            <Button>Vender Ahora</Button>
+          </div>
+        </div>
       </section>
       <section className="grid gap-y-4 mx-auto">
         <div className="w-48 h-64">
